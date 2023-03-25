f1d33be248c6dabb5fc99ac46ed36fe35a28508f
vishalchichani <vishalchichani@gmail.com>
2023-03-26 01:41:45 +0530

    static layout done

--- roll-the-dice/src/App.js
+++ roll-the-dice/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import Dice from './Dice';
 
 function App() {
     return (
+      <div>
       <div className='container'>
-          
+        <h1> Roll the Dice!</h1>
+          <div className='dice-box'>
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          </div>
+      </div>
+      <button className='btn'> ROLL </button>
       </div>
     )
 }
---
+++ roll-the-dice/src/Dice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dice(){
+    return(
+
+            <div className='dice'> 
+                <h2> 1 </h2>
+            </div>
+        
+    )
+}
+export default Dice;
