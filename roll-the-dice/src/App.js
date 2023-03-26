4e304ab09d2f5daaa035d313a17b45ef9a0ebc92
vishalchichani <vishalchichani@gmail.com>
2023-03-27 02:48:10 +0530

    button functionality added

--- roll-the-dice/src/App.js
+++ roll-the-dice/src/App.js
@@ -9,22 +9,33 @@ function App() {
       }
       return diceValue; 
     }
-
-    const newDice = generateDice().map(item => {
+    
+    const [dice, setDice] = React.useState(generateDice());
+    
+    let newDice =[];
+    newDice=dice.map(item => {
       return (
         <Dice value={item} />
       )
     })
 
+    function rollTheDice(){
+      setDice(generateDice());
+    }
+   
+
     return (
       <div>
       <div className='container'>
-        <h1> Roll the Dice!</h1>
+        <div className='head'>
+          <h1> Roll the Dice!</h1>
+          <p> Freeze all the dice at same number in minimum number of moves. Make your choice wisely!</p>
+        </div>
           <div className='dice-box'>
             {newDice}
           </div>
       </div>
-      <button className='btn'> ROLL </button>
+      <button className='btn' onClick={rollTheDice}> ROLL </button>
       </div>
     )
 }
