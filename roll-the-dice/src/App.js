7e4050d5b727605cb9a3ce83f8fb1987125bd85d
vishalchichani <vishalchichani@gmail.com>
2023-03-27 06:40:07 +0530

    style change property

--- roll-the-dice/src/App.js
+++ roll-the-dice/src/App.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import Dice from './Dice';
-
+import {nanoid} from 'nanoid';
 function App() {
-    function generateDice(){
-      let diceValue=[];
-      for(let i=0; i<10;i++){
-        diceValue.push( {value: Math.ceil(Math.random()*6), isHold:false});
-      }
-      return diceValue; 
-    }
+    
     
     const [dice, setDice] = React.useState(generateDice());
     
-   
-
-    let newDice =[];
-    newDice=dice.map(item => {
-      return (
-        <Dice value={item.value} isHold={item.isHold} />
-      )
-    })
+    function generateDice(){
+      let diceValue=[];
+      for(let i=0; i<10;i++){
+        diceValue.push( {id: nanoid(), value: Math.ceil(Math.random()*6), isHold:false});
+      }
+      return diceValue; 
+    }
 
     function rollTheDice(){
       setDice(generateDice());
     }
+    
+    function handleClick(id){
+        setDice(prevState => prevState.map(items => {
+              return items.id === id ? {...items, isHold: !items.isHold} :items
+            })
+        )
+    }
+
+   
+  
+
    
 
+    
+    const newDice=dice.map(item => (
+        <Dice 
+        key= {item.id}
+        value={item.value} 
+        isHold={item.isHold} 
+        handleclick={() => handleClick(item.id)} 
+        />
+    ))
+
+    
     return (
       <div>
       <div className='container'>
--- roll-the-dice/src/Dice.js
+++ roll-the-dice/src/Dice.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Dice(props){
 
-    const style = {
+    const styles = {
         backgroundColor: props.isHold ? "goldenrod" : "white"
     }
 
     return(
 
-            <div className='dice' style={style}> 
+            <div className='dice' style={styles} onClick={props.handleclick}> 
                 <h2> {props.value} </h2>
             </div>
         
