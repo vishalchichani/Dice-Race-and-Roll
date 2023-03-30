9a82c3c175c5f265ecc2d2e0f2b87f8211531259
vishalchichani <vishalchichani@gmail.com>
2023-03-30 20:28:52 +0530

    Minor Fixes

--- roll-the-dice/src/Dice.js
+++ roll-the-dice/src/Dice.js
@@ -1,17 +1,13 @@
 import React from 'react'
 
 function Dice(props){
-
     const styles = {
         backgroundColor: props.isHold ? "goldenrod" : "white"
     }
-
     return(
-
-            <div className='dice' style={styles} onClick={props.handleclick}> 
-                <h2> {props.value} </h2>
-            </div>
-        
+        <div className='dice' style={styles} onClick={props.handleclick}> 
+            <h2> {props.value} </h2>
+        </div>        
     )
 }
 export default Dice;
